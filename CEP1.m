59136f841660498a25996f1bacf96e402a292c95
Mateus Emanuel Mamani Araújo <20191CECA70093@cefetes.br>
2022-08-24 19:18:39 -0300

    distribuição normal padrão z

---
+++ CEP1.m
@@ -0,0 +1,6 @@
+a = 50;
+b = 100;
+r = (b-a)*rand(1000,1) + a;
+%display(r);
+mean(r);
+std(r);
---
+++ CEP2.m
@@ -0,0 +1,6 @@
+tempo_medio = 45;
+desvio = 20;
+amostra = 50;
+
+trashhold_superior_tempo = 50;
+trashhold_inferiror_tempo = 30;
